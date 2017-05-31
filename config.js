2ef043b2972d0f5c278dce584202f3323d73719f
Joe Enos <joe@jtenos.com>
2017-05-31 20:20:15 +0000

    Adding access keys

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 "use strict";
 
 module.exports = {
-    awsRegion: "us-east-2"
+    awsRegion: "us-east-2",
+    accessKeyId: "AKIAI73UZ646SON2HIUA",
+    secretAccessKey: "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"
 };
